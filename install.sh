f0a03de5e90ccdb6752aa0ef4916ed6fc8a2f9f2
Robert <rromanowicz@hotmail.com>
2023-03-21 20:01:58 +0100

    Installation script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PWD=$(pwd)"/.config"
+CONF_DIR=~/.config
+
+function create_symlink {
+	TRGT=$CONF_DIR"/$1"
+	SRC=$PWD"/$1"
+	[[ -d $CONF_DIR"/$1" ]] && mv $TRGT $TRGT".old"
+	ln -sf $SRC $TRGT
+}
+
+APPS=("bashtop" "kitty" "ranger" "polybar" "nvim")
+for i in "${APPS[@]}"
+do
+	echo "Adding $i configuration."
+	create_symlink "$i"
+done
+
+echo ""
+echo "-----------------------------"
+echo "All done."
