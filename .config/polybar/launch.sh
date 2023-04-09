2227ff667859589dd93b155dc78942e3d4ad2e9d
Robert <rromanowicz@hotmail.com>
2023-04-09 17:30:13 +0200

    Updates

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,6 @@ THEME="v2"
 killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-CONFIG_DIR=~/.config/polybar/themes/$THEME/config.ini
+CONFIG_DIR=~/git/dotfiles/.config/polybar/themes/$THEME/config.ini
 polybar primary -c $CONFIG_DIR &
 polybar secondary -c $CONFIG_DIR &
--- .config/polybar/scripts/vpnstatus.sh
+++ .config/polybar/scripts/vpnstatus.sh
@@ -3,7 +3,7 @@
 status=$(nordvpn status | grep "Status\|Country" | sed 's/.*: //' | sed 'N;s/\n/: /' | sed 's/Connected: //')
 
 if [[ $status == "Disconnected" ]]; then
-  echo '%{F#f00} %{F-}------'
+  echo '%{F#FF7575} %{F-}------'
 else
-  echo %{F#0f0} %{F-}$status
+  echo %{F#96F7C8} %{F-}$status
 fi
