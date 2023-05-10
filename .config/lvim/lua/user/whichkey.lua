942597066f52ca65d9c6c7fbc960291381c6ff84
rromanowicz <rromanowicz@hotmail.com>
2023-05-10 16:47:20 +0200

    lvim keybindings update

--- .config/lvim/lua/user/whichkey.lua
+++ .config/lvim/lua/user/whichkey.lua
@@ -1,16 +1,3 @@
-lvim.builtin.which_key.mappings["B"] = { "<cmd>Telescope buffers<cr>", "Buffers" }
-lvim.builtin.which_key.mappings["v"] = { "<cmd>vsplit<cr>", "vsplit" }
-lvim.builtin.which_key.mappings["h"] = { "<cmd>nohlsearch<cr>", "nohl" }
-lvim.builtin.which_key.mappings["q"] = { '<cmd>lua require("user.functions").smart_quit()<CR>', "Quit" }
-lvim.builtin.which_key.mappings["/"] = { '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', "Comment" }
--- lvim.builtin.which_key.mappings["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" }
-lvim.builtin.which_key.mappings["gy"] = "Link"
-lvim.builtin.which_key.mappings["r"] = {
-  name = "Replace",
-  r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
-  w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
-  f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
-}
 lvim.builtin.which_key.mappings["D"] = {
   name = "Debug",
   b = { "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Breakpoint" },
@@ -24,65 +11,14 @@ lvim.builtin.which_key.mappings["D"] = {
   x = { "<cmd>lua require'dap'.terminate()<cr>", "Exit" },
   a = { function() local dap = require('dap') dap.configurations.java = { { type = 'java'; request = 'attach'; name = "Java Debug (Attach) - Remote"; hostName = "127.0.0.1"; port = 5005; }, } dap.continue() end, "Attach debug session." }
 }
-lvim.builtin.which_key.mappings["d"] = { 
+lvim.builtin.which_key.mappings["d"] = {
   name = "[D]efinitions",
   h = { vim.lsp.buf.hover, "Hover Documentation" },
   d = { vim.lsp.buf.definition, "[D]efinition" },
-  D = { vim.lsp.buf.type_definition, "Type [D]efinition" }, 
+  D = { vim.lsp.buf.type_definition, "Type [D]efinition" },
   r = { "<cmd>TroubleToggle lsp_references<cr>", "References" },
-  s = { vim.lsp.buf.signature_help, "[S]ignature Documentation" },
+  S = { vim.lsp.buf.signature_help, "[S]ignature Documentation" },
   s = { require('telescope.builtin').lsp_document_symbols, "[D]ocument [S]ymbols" },
-}
-lvim.builtin.which_key.mappings["f"] = {
-  name = "Find",
-  b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-  c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
-  f = { "<cmd>Telescope find_files<cr>", "Find files" },
-  t = { "<cmd>Telescope live_grep<cr>", "Find Text" },
-  s = { "<cmd>Telescope grep_string<cr>", "Find String" },
-  h = { "<cmd>Telescope help_tags<cr>", "Help" },
-  H = { "<cmd>Telescope highlights<cr>", "Highlights" },
-  i = { "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", "Media" },
-  l = { "<cmd>Telescope resume<cr>", "Last Search" },
-  M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
-  r = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
-  R = { "<cmd>Telescope registers<cr>", "Registers" },
-  k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
-  C = { "<cmd>Telescope commands<cr>", "Commands" },
-}
-lvim.builtin.which_key.mappings["g"] = {
-  name = "Git",
-  -- g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle(12000)<cr>", "Lazygit" },
-  g = { "<cmd>Neogit<cr>", "Neogit" },
-  j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-  k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-  l = { "<cmd>GitBlameToggle<cr>", "Blame" },
-  p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-  r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-  R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-  s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-  u = {
-    "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-    "Undo Stage Hunk",
-  },
-  n = { ":!git checkout -b ", "Checkout New Branch" },
-  o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-  b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-  c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-  f = { "<cmd>Telescope git_bcommits<cr>", "Checkout buffer commit" },
-  d = {
-    "<cmd>Gitsigns diffthis HEAD<cr>",
-    "Diff",
-  },
-  G = {
-    name = "Gist",
-    a = { "<cmd>Gist -b -a<cr>", "Create Anon" },
-    d = { "<cmd>Gist -d<cr>", "Delete" },
-    f = { "<cmd>Gist -f<cr>", "Fork" },
-    g = { "<cmd>Gist -b<cr>", "Create" },
-    l = { "<cmd>Gist -l<cr>", "List" },
-    p = { "<cmd>Gist -b -p<cr>", "Create Private" },
-  },
 }
 lvim.builtin.which_key.mappings["l"] = {
   name = "LSP",
@@ -91,11 +27,11 @@ lvim.builtin.which_key.mappings["l"] = {
   d = { "<cmd>TroubleToggle<cr>", "Diagnostics" },
   w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace Diagnostics", },
   f = { "<cmd>lua vim.lsp.buf.format({ async = true })<cr>", "Format" },
-  F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
+  -- F = { "<cmd>LspToggleAutoFormat<cr>", "Toggle Autoformat" },
   i = { "<cmd>LspInfo<cr>", "Info" },
   h = { "<cmd>lua require('lsp-inlayhints').toggle()<cr>", "Toggle Hints" },
-  H = { "<cmd>IlluminationToggle<cr>", "Toggle Doc HL" },
-  I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+  -- H = { "<cmd>IlluminationToggle<cr>", "Toggle Doc HL" },
+  -- I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
   j = {
     "<cmd>lua vim.diagnostic.goto_next({buffer=0})<CR>",
     "Next Diagnostic",
@@ -140,22 +76,88 @@ lvim.builtin.which_key.mappings["o"] = {
   -- t = { '<cmd>lua require("user.functions").toggle_tabline()<cr>', "Tabline" },
 }
 
-lvim.builtin.which_key.mappings["n"] = {
-  name = "Notes",
-  c = { "<cmd>Telekasten show_calendar<cr>", "Calendar" },
-  n = { "<cmd>Telekasten new_note<cr>", "Note" },
-  f = { "<cmd>Telekasten find_notes<cr>", "Find" },
-  F = { "<cmd>Telekasten find_daily_notes<cr>", "Find Journal" },
-  j = { "<cmd>Telekasten goto_today<cr>", "Journal" },
-  p = { "<cmd>Telekasten panel<cr>", "Panel" },
-  t = { "<cmd>Telekasten toggle_todo<cr>", "Toggle Todo" },
-}
 
-lvim.builtin.which_key.mappings[";"] = nil
+-- lvim.builtin.which_key.mappings["f"] = {
+--   name = "Find",
+--   b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+--   c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+--   f = { "<cmd>Telescope find_files<cr>", "Find files" },
+--   t = { "<cmd>Telescope live_grep<cr>", "Find Text" },
+--   s = { "<cmd>Telescope grep_string<cr>", "Find String" },
+--   h = { "<cmd>Telescope help_tags<cr>", "Help" },
+--   H = { "<cmd>Telescope highlights<cr>", "Highlights" },
+--   i = { "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", "Media" },
+--   l = { "<cmd>Telescope resume<cr>", "Last Search" },
+--   M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+--   r = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
+--   R = { "<cmd>Telescope registers<cr>", "Registers" },
+--   k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+--   C = { "<cmd>Telescope commands<cr>", "Commands" },
+-- }
+-- lvim.builtin.which_key.mappings["g"] = {
+--   name = "Git",
+--   -- g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle(12000)<cr>", "Lazygit" },
+--   g = { "<cmd>Neogit<cr>", "Neogit" },
+--   j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+--   k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+--   l = { "<cmd>GitBlameToggle<cr>", "Blame" },
+--   p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+--   r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+--   R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+--   s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+--   u = {
+--     "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+--     "Undo Stage Hunk",
+--   },
+--   n = { ":!git checkout -b ", "Checkout New Branch" },
+--   o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+--   b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+--   c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+--   f = { "<cmd>Telescope git_bcommits<cr>", "Checkout buffer commit" },
+--   d = {
+--     "<cmd>Gitsigns diffthis HEAD<cr>",
+--     "Diff",
+--   },
+--   G = {
+--     name = "Gist",
+--     a = { "<cmd>Gist -b -a<cr>", "Create Anon" },
+--     d = { "<cmd>Gist -d<cr>", "Delete" },
+--     f = { "<cmd>Gist -f<cr>", "Fork" },
+--     g = { "<cmd>Gist -b<cr>", "Create" },
+--     l = { "<cmd>Gist -l<cr>", "List" },
+--     p = { "<cmd>Gist -b -p<cr>", "Create Private" },
+--   },
+-- }
+
+-- lvim.builtin.which_key.mappings["n"] = {
+--   name = "Notes",
+--   c = { "<cmd>Telekasten show_calendar<cr>", "Calendar" },
+--   n = { "<cmd>Telekasten new_note<cr>", "Note" },
+--   f = { "<cmd>Telekasten find_notes<cr>", "Find" },
+--   F = { "<cmd>Telekasten find_daily_notes<cr>", "Find Journal" },
+--   j = { "<cmd>Telekasten goto_today<cr>", "Journal" },
+--   p = { "<cmd>Telekasten panel<cr>", "Panel" },
+--   t = { "<cmd>Telekasten toggle_todo<cr>", "Toggle Todo" },
+-- }
+
+-- lvim.builtin.which_key.mappings[";"] = nil
 -- lvim.builtin.which_key.mappings["c"] = nil
-lvim.builtin.which_key.mappings["L"] = nil
-lvim.builtin.which_key.mappings["s"] = nil
-lvim.builtin.which_key.mappings["w"] = nil
+-- lvim.builtin.which_key.mappings["L"] = nil
+-- lvim.builtin.which_key.mappings["s"] = nil
+-- lvim.builtin.which_key.mappings["w"] = nil
+-- lvim.builtin.which_key.mappings["B"] = { "<cmd>Telescope buffers<cr>", "Buffers" }
+-- lvim.builtin.which_key.mappings["v"] = { "<cmd>vsplit<cr>", "vsplit" }
+-- lvim.builtin.which_key.mappings["h"] = { "<cmd>nohlsearch<cr>", "nohl" }
+-- lvim.builtin.which_key.mappings["q"] = { '<cmd>lua require("user.functions").smart_quit()<CR>', "Quit" }
+-- lvim.builtin.which_key.mappings["/"] = { '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', "Comment" }
+-- lvim.builtin.which_key.mappings["c"] = { "<cmd>Bdelete!<CR>", "Close Buffer" }
+-- lvim.builtin.which_key.mappings["gy"] = "Link"
+-- lvim.builtin.which_key.mappings["r"] = {
+--   name = "Replace",
+--   r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
+--   w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
+--   f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
+-- }
 
 local m_opts = {
   mode = "n", -- NORMAL mode
@@ -172,23 +174,23 @@ if not status_ok then
 end
 
 local m_mappings = {
-  a = { "<cmd>silent BookmarkAnnotate<cr>", "Annotate" },
-  c = { "<cmd>silent BookmarkClear<cr>", "Clear" },
-  b = { "<cmd>silent BookmarkToggle<cr>", "Toggle" },
-  m = { '<cmd>lua require("harpoon.mark").add_file()<cr>', "Harpoon" },
-  ["."] = { '<cmd>lua require("harpoon.ui").nav_next()<cr>', "Harpoon Next" },
-  [","] = { '<cmd>lua require("harpoon.ui").nav_prev()<cr>', "Harpoon Prev" },
-  l = { "<cmd>lua require('user.bfs').open()<cr>", "Buffers" },
+  -- a = { "<cmd>silent BookmarkAnnotate<cr>", "Annotate" },
+  -- c = { "<cmd>silent BookmarkClear<cr>", "Clear" },
+  -- b = { "<cmd>silent BookmarkToggle<cr>", "Toggle" },
+  -- m = { '<cmd>lua require("harpoon.mark").add_file()<cr>', "Harpoon" },
+  -- ["."] = { '<cmd>lua require("harpoon.ui").nav_next()<cr>', "Harpoon Next" },
+  -- [","] = { '<cmd>lua require("harpoon.ui").nav_prev()<cr>', "Harpoon Prev" },
+  -- l = { "<cmd>lua require('user.bfs').open()<cr>", "Buffers" },
   -- j = { "<cmd>silent BookmarkNext<cr>", "Next" },
-  s = { "<cmd>Telescope harpoon marks<cr>", "Search Files" },
-  k = { "<cmd>silent BookmarkPrev<cr>", "Prev" },
-  S = { "<cmd>silent BookmarkShowAll<cr>", "Prev" },
+  -- s = { "<cmd>Telescope harpoon marks<cr>", "Search Files" },
+  -- k = { "<cmd>silent BookmarkPrev<cr>", "Prev" },
+  -- S = { "<cmd>silent BookmarkShowAll<cr>", "Prev" },
   -- s = {
   --   "<cmd>lua require('telescope').extensions.vim_bookmarks.all({ hide_filename=false, prompt_title=\"bookmarks\", shorten_path=false })<cr>",
   --   "Show",
   -- },
-  x = { "<cmd>BookmarkClearAll<cr>", "Clear All" },
-  [";"] = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', "Harpoon UI" },
+  -- x = { "<cmd>BookmarkClearAll<cr>", "Clear All" },
+  -- [";"] = { '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', "Harpoon UI" },
 }
 
 which_key.register(m_mappings, m_opts)
