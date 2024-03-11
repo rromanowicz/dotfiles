06353e6227932b4ad38144d19b2c08359fbecc01
rromanowicz <80149660+rromanowicz@users.noreply.github.com>
2024-03-11 10:10:30 +0100

    docker script

---
+++ scripts/install_docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#docker run -it --privileged=true archlinux:latest /bin/bash 
+#
+
+
+pacman -Sy
+pacman -S --noconfirm git neovim make python cargolazygit npm unzip awesome-terminal-fonts rust rust-src
+
+if [ ! -d "$HOME/.jdk/" ]; then
+  echo "Creating ~/.jdk/ directory."
+  mkdir ~/.jdk && cd ~/.jdk/
+fi
+
+if [ ! -d "$HOME/.jdk/jdk-21/" ]; then
+  cd ~/.jdk/
+  JDK_21=openjdk-21_linux-x64_bin.tar.gz
+  wget https://download.java.net/java/GA/jdk21/fd2272bbf8e04c3dbaee13770090416c/35/GPL/$JDK_21
+  tar -xzvf $JDK_21
+  rm $JDK_21
+fi
+
+git clone https://github.com/rromanowicz/NvChad ~/.config/nvim --depth 1 && nvim
