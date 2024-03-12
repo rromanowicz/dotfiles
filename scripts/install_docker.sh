a71aeed1725a540a624295906d7b0dd8d34e8419
rromanowicz <80149660+rromanowicz@users.noreply.github.com>
2024-03-12 10:57:32 +0100

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#docker run -it --privileged=true archlinux:latest /bin/bash 
-#pacman -Sy && pacman -S --noconfirm wget && wget -O - https://raw.githubusercontent.com/rromanowicz/dotfiles/master/scripts/install_docker.sh | bash
+#docker build --no-cache --tag 'devenv:latest' .
+#docker run --name "devenv" -it devenv /bin/zsh
+
 
 pacman -S --noconfirm git zsh neovim make python cargo lazygit npm unzip awesome-terminal-fonts rust rust-src ripgrep 
 
@@ -9,11 +10,18 @@ pacman -S --noconfirm git zsh neovim make python cargo lazygit npm unzip awesome
 echo "Setting up zsh."
 chsh -s /bin/zsh
 
+
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+
+git clone http://github.com/rromanowicz/dotfiles $HOME/git/dotfiles --depth 1 --no-checkout
+
+git clone --depth 1 --branch master --single-branch http://github.com/rromanowicz/dotfiles $HOME/.oh-my-zsh
+echo "alias vim='nvim'" >> $HOME/.zshrc
+echo "alias lg='lazygit'" >> $HOME/.zshrc
 
 
 echo "Setting up JDK."
@@ -30,10 +38,10 @@ if [ ! -d "$HOME/.jdk/jdk-21/" ]; then
   rm $JDK_21
 fi
 
+
 echo "Cloning nvim config."
 git clone https://github.com/rromanowicz/NvChad ~/.config/nvim --depth 1
 
 
-echo "Setting up ZSH."
-echo "alias vim='nvim'" >> $HOME/.zshrc
-echo "alias lg='lazygit'" >> $HOME/.zshrc
+
+mkdir $HOME/git
